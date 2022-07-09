5eec6c92001389e19c4fde6e7463667931bc4504
potnet <69406221+potnet@users.noreply.github.com>
2022-07-09 04:12:10 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "test",
-    usage: "test",
-    description: "test command",
-    run(client, message, args, time) {
-
-        message.channel.send("this is a test")
-    }
-}
